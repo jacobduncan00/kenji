f31d0eefdf03ae3b4a5ee2ec70d522367223efb6
Jacob Duncan <jacobduncan00@yahoo.com>
2022-12-20 22:07:38 -0500

    remove creds

--- context/appContext.tsx
+++ context/appContext.tsx
@@ -4,9 +4,8 @@ import { createClient } from "@supabase/supabase-js";
 const AppContext = createContext({});
 
 const AppContextProvider = ({ children }: any) => {
-  const supabaseUrl = "https://fbvyilbpylygpxpgivip.supabase.co";
-  const supabaseKey =
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZidnlpbGJweWx5Z3B4cGdpdmlwIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzExMzc3ODUsImV4cCI6MTk4NjcxMzc4NX0.YElNH__KZT0-9YxW2BkRTZbvDgJxUNmUDoJcVvWSXD0";
+  const supabaseUrl = "";
+  const supabaseKey = "";
   const supabase = createClient(supabaseUrl!, supabaseKey!);
 
   return (
