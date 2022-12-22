ba01c550efb60be4905d847ab49fcb17cf62f190
Jacob Duncan <jacobduncan00@yahoo.com>
2022-12-22 17:29:21 -0500

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,9 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
-import { AppContextProvider } from "../context/appContext";
+// import { AppContextProvider } from "../context/appContext";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <AppContextProvider>
       <Component {...pageProps} />
-    </AppContextProvider>
   );
 }
